d0e0aee8de6ce066d5aca73ebc705a7320f5a8e9
xlez18x <lezy17@gmail.com>
2015-04-12 16:33:42 -0430

    Commit agregacion de true y false al lexico
    Gramatica del true y false en factor
    
    Gramatica del return

--- src/ast/NodoValor.java
+++ src/ast/NodoValor.java
@@ -2,18 +2,28 @@ package ast;
 
 public class NodoValor extends NodoBase {
 	private Integer valor;
+	private Boolean valorb;
 
 	public NodoValor(Integer valor) {
 		super();
 		this.valor = valor;
 	}
 
+	public NodoValor(Boolean valor) {
+		super();
+		this.valorb = valor;
+	}
+
 	public NodoValor() {
 		super();
 	}
 	
-	public Integer getValor() {
+	public Integer getValorInteger() {
 		return valor;
+	}
+
+	public Boolean getValorBoolean() {
+		return valorb;
 	}
 
 }
