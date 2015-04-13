a4a4d9a6f64f952149a5ee8218f458bd96a06684
Luis Alfredo da Silva <luisdasilva@covetel.com.ve>
2015-04-13 17:50:10 -0430

    Cambios:     - Se cambió la codificación de varios archivos a UTF-8 por algunos     carácteres     - Se agregó al script de compilación un bloque para ejecución desde     consola y enviar nombre de archivo por parámetro

--- run_generadores.sh
+++ run_generadores.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-echo "# =================================================================== #"
-echo "# ===========Generando analizador lexico: (JFLEX)==================== #"
-echo "# =================================================================== #"
-java -jar JAR/JFlex.jar -d src/compilador especificacion_lexica/lexico.flex
-echo "# =================================================================== #"
-echo "# ===============Generando analizador sintactico: (CUP)============== #"
-echo "# =================================================================== #"
-java -jar JAR/java-cup-11a.jar -destdir src/compilador/ especificacion_sintactica/sintactico.cup
+
+case "$1" in 
+    "compile" )
+        echo "# =================================================================== #"
+        echo "# ===========Generando analizador lexico: (JFLEX)==================== #"
+        echo "# =================================================================== #"
+        java -jar JAR/JFlex.jar -d src/compilador especificacion_lexica/lexico.flex
+        echo "# =================================================================== #"
+        echo "# ===============Generando analizador sintactico: (CUP)============== #"
+        echo "# =================================================================== #"
+        java -jar JAR/java-cup-11a.jar -destdir src/compilador/ especificacion_sintactica/sintactico.cup
+    ;;
+
+    "run" )
+        javac -d build/classes -classpath src:compilador:src/ast:JAR/java-cup-11a-runtime.jar src/compilador/Compilador.java
+        java -cp build/classes:JAR/java-cup-11a-runtime.jar compilador.Compilador $2
+    ;;
+
+esac
