e7111db81b07160cbad9e267a7dc39754eeaa1a1
ebar0n <edwar.baron@gmail.com>
2015-04-12 22:48:42 -0430

    Cambios:
    1) Modificacion de nodo asignacion

--- src/ast/NodoAsignacion.java
+++ src/ast/NodoAsignacion.java
@@ -2,6 +2,7 @@ package ast;
 
 public class NodoAsignacion extends NodoBase {
 	private String identificador;
+	private NodoBase vector;
 	private NodoBase expresion;
 	
 	public NodoAsignacion(String identificador) {
@@ -13,6 +14,18 @@ public class NodoAsignacion extends NodoBase {
 	public NodoAsignacion(String identificador, NodoBase expresion) {
 		super();
 		this.identificador = identificador;
+		this.expresion = expresion;
+	}
+
+	public NodoAsignacion(NodoBase vector) {
+		super();
+		this.vector = vector;
+		this.expresion = null;
+	}
+	
+	public NodoAsignacion(NodoBase vector, NodoBase expresion) {
+		super();
+		this.vector = vector;
 		this.expresion = expresion;
 	}
 
