430ac39e9732bd14ae67d22635952b9feb8007a8
cristopher caceres <cristopher.cc92@gmail.com>
2015-04-15 00:22:21 -0430

    Agregando nodo de argumento de lista

---
+++ src/ast/NodoArgList.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ast;
+
+/**
+ *
+ * @author Cristopher_Notebook
+ */
+public class NodoArgList extends NodoBase {
+    
+    private String identificador;
+    private tipoDato tipo;
+    private NodoBase argumento;
+
+    public NodoArgList(String identificador,tipoDato tipo) {
+        super();
+        this.identificador = identificador;
+        this.tipo = tipo;
+    }
+
+    public NodoArgList(String identificador, tipoDato tipo, NodoBase argumento) {
+        this.identificador = identificador;
+        this.tipo = tipo;
+        this.argumento = argumento;
+    }
+      
+
+    public void setArgumento(NodoBase argumento) {
+        this.argumento = argumento;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public void setTipo(tipoDato tipo) {
+        this.tipo = tipo;
+    }
+
+    public NodoBase getArgumento() {
+        return argumento;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public tipoDato getTipo() {
+        return tipo;
+    }
+    
+}
