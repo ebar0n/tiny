cf2fbff0d135f5f492e97d9a8f9c8726b2e2d97c
cristopher caceres <cristopher.cc92@gmail.com>
2015-04-15 00:32:56 -0430

    Modificando nodo de argumentos de funcion (llamada)

--- src/ast/NodoParamFunction.java
+++ src/ast/NodoParamFunction.java
@@ -2,65 +2,33 @@ package ast;
 
 public class NodoParamFunction extends NodoBase{
 
-	private String identificador;
+	private NodoBase expresion;
 	private NodoBase base;
-	private int numero;
-	private boolean condicion;
-	
-	public NodoParamFunction(String identificador) {
-		// TODO Auto-generated constructor stub
-		super();
-		this.identificador = identificador;
-	}
-	
-	public NodoParamFunction(NodoBase base) {
-		// TODO Auto-generated constructor stub
-		super();
-		this.base = base;
-	}
-	
-	public NodoParamFunction(int numero) {
-		// TODO Auto-generated constructor stub
-		super();
-		this.numero = numero;
-	}
-	
-	public NodoParamFunction(boolean condicion) {
-		// TODO Auto-generated constructor stub
-		super();
-		this.condicion = condicion;
-	}
-	
-	public void setCondicion(boolean condicion) {
-		this.condicion = condicion;
-	}
-	
-	public void setIdentificador(String identificador) {
-		this.identificador = identificador;
-	}
-	
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	
-	public String getIdentificador() {
-		return identificador;
-	}
-	
-	public int getNumero() {
-		return numero;
-	}
-	
-	public boolean isCondicion() {
-		return condicion;
-	}
-	
-	public void setBase(NodoBase base) {
-		this.base = base;
-	}
-	
-	public NodoBase getBase() {
-		return base;
-	}
-	
+
+        public NodoParamFunction(NodoBase expresion) {
+            super();
+            this.expresion = expresion;
+        }
+        
+        public NodoParamFunction(NodoBase expresion, NodoBase base) {
+            super();
+            this.expresion = expresion;
+            this.base = base;
+        }
+
+        public void setBase(NodoBase base) {
+            this.base = base;
+        }
+
+        public NodoBase getBase() {
+            return base;
+        }
+
+        public void setExpresion(NodoBase expresion) {
+            this.expresion = expresion;
+        }
+
+        public NodoBase getExpresion() {
+            return expresion;
+        }	
 }
