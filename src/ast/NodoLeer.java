a4fedd44ad4c1b67819b97d69c9783c2af614296
cristopher caceres <cristopher.cc92@gmail.com>
2015-04-15 00:07:35 -0430

    Cambios  en .cup de nodos e Id.toString();

--- src/ast/NodoLeer.java
+++ src/ast/NodoLeer.java
@@ -2,12 +2,26 @@ package ast;
 
 public class NodoLeer extends NodoBase {
 	private String id;
+        private NodoArray array;
 
 	public NodoLeer(String identificador) {
 		super();
 		this.id = identificador;
 	}
+        
+        public NodoLeer(NodoBase array){
+                super();
+                this.array = (NodoArray) array;
+        }
 
+        public void setArray(NodoArray array) {
+                this.array = array;
+        }
+
+        public NodoArray getArray() {
+                return array;
+        }
+ 
 	public NodoLeer() {
 		super();
 		id="";
