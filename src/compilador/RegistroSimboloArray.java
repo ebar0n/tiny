53fabcb50ffc02a641817cf7ec65f41cc811bcec
cristopher caceres <cristopher.cc92@gmail.com>
2015-04-16 16:32:16 -0430

    Constructores de registros de simbolos

--- src/compilador/RegistroSimboloArray.java
+++ src/compilador/RegistroSimboloArray.java
@@ -7,13 +7,15 @@ public class RegistroSimboloArray extends RegistroSimbolo {
 	private int NumElementos;
 	private int LimInferior;
 	private int LimSuperior;
+
+        public RegistroSimboloArray(int NumElementos, int LimInferior, int LimSuperior, String identificador, tipoDato tipo, int numLinea, int direccionMemoria) {
+            super(identificador, tipo, numLinea, direccionMemoria);
+            this.NumElementos = NumElementos;
+            this.LimInferior = LimInferior;
+            this.LimSuperior = LimSuperior;
+        }
 	
-	public RegistroSimboloArray(int numElementos,String limInferior, int limSuperior) {
-		super();
-		this.NumElementos = numElementos;
-		this.LimInferior = limInferior;
-		this.LimSuperior = limSuperior;
-	}
+	
 
 	public int getNumElementos() {
 		return this.NumElementos;
