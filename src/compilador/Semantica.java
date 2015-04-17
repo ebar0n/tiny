96ba7a1b6f06c75e7bf01e366231bb92bebdee4d
gregori <gregori.gonzalez.90@gmail.com>
2015-04-16 22:07:45 -0430

    Estructura Clase Semantica.java

---
+++ src/compilador/Semantica.java
@@ -0,0 +1,19 @@
+package compilador;
+
+import ast.*;
+
+
+public class Semantica {
+
+	TablaSimbolos ts;
+	
+	public Semantica(){
+		ts = new TablaSimbolos();
+	}
+
+	public void RecorrerArbol(NodoBase raiz){
+		while (raiz != null) {
+				
+		}
+	}
+}
